eca89740e253c935fdf4034a5d14f5ccf365d688
Antoine <antoine.ngu@outlook.fr>
2019-06-14 21:29:10 +0200

    Update wp-config.php

--- www/wp-config.php
+++ www/wp-config.php
@@ -22,13 +22,13 @@
 define( 'DB_NAME', 'observant' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'observant' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'observantpass' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
